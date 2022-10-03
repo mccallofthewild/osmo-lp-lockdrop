d2f7cb3c303c93cb26bf5917a9901d67c69ab44f
McCall Alexander <mccallalexander96@gmail.com>
2022-10-03 11:58:58 -0500

    note

--- distribute.sh
+++ distribute.sh
@@ -1,3 +1,4 @@
+# creates reward distributors for _all_ tokens in contract 
 beaker wasm execute osmo-lp-lockdrop --signer-account test4 --raw '{ "distribute_all_tokens": {} }' --network testnet 
 
 beaker wasm query osmo-lp-lockdrop --network testnet --raw '{ "all_reward_contracts": {}  }'
